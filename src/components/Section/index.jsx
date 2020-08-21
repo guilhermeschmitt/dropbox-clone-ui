64157cfc3946abd94d71b65fb0544ffd4593133a
Guilherme <guilherme.schmitt@alkasoft.com.br>
2020-08-21 10:58:56 -0300

    Add header and clip style

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
-import { Container, Content } from './styles';
+import {
+  Container,
+  Content,
+  HeaderWrapper,
+  Header,
+  DropboxLogo
+} from './styles';
 
 const Section = ({ title, color, description }) => {
   return (
     <Container className={color}>
+      <HeaderWrapper>
+        <Header>
+          <h1>
+            <DropboxLogo />
+            <span>
+              Dropbox
+            </span>
+          </h1>
+          <button>
+            Acessar
+          </button>
+        </Header>
+      </HeaderWrapper>
+
       <Content>
         <h2>{title}</h2>
         <p>{description}</p>
